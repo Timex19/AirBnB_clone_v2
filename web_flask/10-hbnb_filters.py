53934e072cf9e761f937a1631c5b640a7b76d133
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-08 16:55:05 +0100

    Adding Flask with Hbnb GUI

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -11,14 +11,19 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_session(exception):
-        """ Teardown """
-            storage.close()
+    """ Teardown """
+    storage.close()
 
 
-            @app.route('/hbnb_filters/', strict_slashes=False)
-            def display_html():
-                    """ Function called with /states route """
-                        states = storage.all(State)
-                            amenities = storage.all(Amenity)
+@app.route('/hbnb_filters/', strict_slashes=False)
+def display_html():
+    """ Function called with /states route """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
 
-                                return render_template('10-hbnb_filters.html',
+    return render_template('10-hbnb_filters.html',
+                           states=states.values(),
+                           amenities=amenities.values())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
