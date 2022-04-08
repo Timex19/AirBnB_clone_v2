c3f906785062cb645cd8ade9f77478adba1b56f3
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-08 16:29:53 +0100

    [Feature]: adding Jinja file that displays States and Cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,12 +11,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_session(exception):
-        """ Teardown """
-            storage.close()
+    """ Teardown """
+    storage.close()
 
 
-            @app.route('/cities_by_states', strict_slashes=False)
-            def display_html():
-                    """ Function called with /states_list route """
-                        states = storage.all(State)
-                            return render_template('8-cities_by_states.html',
+@app.route('/cities_by_states', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html',
+                           Table="States",
+                           states=states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
